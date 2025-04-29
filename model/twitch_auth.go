5e9ca7409226dda4d6952fc81cfa550e99955f85
Sebastian Rakel <sebastian@devunit.eu>
2025-04-29 07:40:09 +0200

    Rewrite Stuff

--- model/twitch_auth.go
+++ model/twitch_auth.go
@@ -1,11 +1,44 @@
 package model
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+)
 
-type TwitchAuth struct {
+type twitchAuth struct {
 	Username              string
 	AccessToken           string
 	AccessTokenValidTill  time.Time
 	RefreshToken          string
 	RefreshTokenValidTill time.Time
+}
+
+type TwitchAuth twitchAuth
+
+func (t *TwitchAuth) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	result := &twitchAuth{}
+	err := json.Unmarshal(bytes, &result)
+	*t = TwitchAuth(*result)
+	return err
+}
+
+func (t TwitchAuth) Value() (driver.Value, error) {
+	if len(t.Username) == 0 {
+		return nil, nil
+	}
+
+	bytes, err := json.Marshal(t)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
 }
--- model/user.go
+++ model/user.go
@@ -1,12 +1,36 @@
 package model
 
 import (
+	"encoding/json"
+
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Identifier    string
-	Username      string
-	TwitchBotAuth TwitchAuth
+	Identifier     string
+	Username       string
+	TwitchUserAuth TwitchAuth
+	TwitchBotAuth  TwitchAuth
+}
+
+func (u *User) SetTwitchUserAuth(data any) error {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	u.Payload = datatypes.JSON(jsonBytes)
+	return nil
+}
+
+func (u *User) GetTwitchUserAuth() (map[string]any, error) {
+	var payload map[string]any
+	jsonBytes, err := e.Payload.MarshalJSON()
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(jsonBytes, &payload)
+	return payload, err
 }
